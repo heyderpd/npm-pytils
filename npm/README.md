47b0a309696462da2a4d5ed5abf9860896c7de80
heyder.dias <heyder.dias@b2wdigital.com>
2017-04-26 18:35:15 -0300

    v1.29.0

--- npm/README.md
+++ npm/README.md
@@ -13,9 +13,16 @@ npm install --save pytils
 ```terminal
 const p = require('pytils')
 
-p.compose  = function ( _arrayOfFunctions ) // return function waiting for input
-p.curry    = function ( _function ) // return function waiting for input
-p.prop     = function ( _arrProps, _object ) // return prop of _object in path or undefined
+p.compose = function ( _arrayOfFunctions ) // return function waiting for input
+p.curry   = function ( _function ) // return function waiting for input
+p.path [curry] = function ( _arrPathOfProps, _object ) // return prop of _object in path or undefined
+p.reduce = ( _object , fx ) // do each of any type, pass to function var's (key, value) to return a value
+
+p.copy    = ( _object ) // copy any types
+p.length  = ( _object ) // get length of any type, return -1 if can't
+p.keys    = ( _object ) // get keys of any type, return [] if can't
+p.values  = ( _object ) // get values of any type, return [] if can't
+p.hasProp = ( _object , prop ) // of any type, return true or false
 
 p.type     = function ( _object ) // return type of var in a string
 p.isType   = function ( _object , _type_need) // return true or false
@@ -24,19 +31,22 @@ p.isNumber = function ( _object ) // return true or false
 p.isArray  = function ( _object ) // return true or false
 p.isObject = function ( _object ) // return true or false
 p.isFunction = function ( _object ) // return true or false
-p.isAOF = function ( _object ) // return true or false if is isArray OR isObject OR isFunction
-p.isNull   = function ( _object ) // return true or false
+p.isAOF  = function ( _object ) // return true or false if is isArray OR isObject OR isFunction
+p.isNull = function ( _object ) // return true or false
 p.isUndefined = function ( _object ) // return true or false
-
-p.copy   = ( _object ) // copy any types
-p.length = ( _object ) // get length of any type, return -1 if can't
-p.keys   = ( _object ) // get keys of any type, return [] if can't
-p.values   = ( _object ) // get values of any type, return [] if can't
-p.hasProp = ( _object , prop ) // of any type, return true or false
-
-p.fx = function( key, value ) { console.log('item: ', keys, ' = ', value) }
-p.map   = ( _object , fx ) // do each of any type, pass to function var's (key, value)
 
 p.toObject = ( _array ) // convert array to object, return object have values of array in keys of object
 p.arrayDiff = ( _array base , _array compare ) // compare two array and return the diff values of base
+p.invertObj = ( _object ) // convert object keys in value and value in keys
+p.uniq = ( _array ) // get unique values of array, work with simple var's
+p.eq1True   = ( _array ) // if is a array of one value of true, return true
+p.uniqWith  = ( _comparator, _array ) // foreach array and return unique itens with comparator
+p.uniqObject  = ( _objectA, _objectB ) // compare all prop of two object and return if is equal
+p.ojbFromVals = ( _array ) // create object from array values
+
+var _function = function( key, value ) { console.log('item: ', keys, ' = ', value) }
+p.map [curry] = ( _object , _function ) // do each of any type, pass to function var's (key, value)
+
+var _dictionary = { toA, 'fromB', toC: 'fromD' }
+p.translate [curry] = ( _dictionary, _array ) // translate object using dictionary of props
 ```
--- npm/index.js
+++ npm/index.js
@@ -1,6 +1,12 @@
 'use strict';
 
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+
 var _typeof = typeof Symbol === "function" && typeof Symbol.iterator === "symbol" ? function (obj) { return typeof obj; } : function (obj) { return obj && typeof Symbol === "function" && obj.constructor === Symbol && obj !== Symbol.prototype ? "symbol" : typeof obj; };
+
+function _toConsumableArray(arr) { if (Array.isArray(arr)) { for (var i = 0, arr2 = Array(arr.length); i < arr.length; i++) { arr2[i] = arr[i]; } return arr2; } else { return Array.from(arr); } }
 
 /*!
  * pytils
@@ -8,47 +14,9 @@ var _typeof = typeof Symbol === "function" && typeof Symbol.iterator === "symbol
  * ISC Licensed
 */
 
-var compose = function compose() {
-  for (var _len = arguments.length, args = Array(_len), _key = 0; _key < _len; _key++) {
-    args[_key] = arguments[_key];
-  }
+/* TYPE */
 
-  return function (input) {
-    return args.reduce(function (obj, func) {
-      return func(obj);
-    }, input);
-  };
-};
-
-var curry = function curry(func) {
-  return function () {
-    for (var _len2 = arguments.length, args = Array(_len2), _key2 = 0; _key2 < _len2; _key2++) {
-      args[_key2] = arguments[_key2];
-    }
-
-    if (args.length >= func.length) {
-      return func.apply(undefined, args);
-    }
-    var store = args;
-    return function () {
-      for (var _len3 = arguments.length, args = Array(_len3), _key3 = 0; _key3 < _len3; _key3++) {
-        args[_key3] = arguments[_key3];
-      }
-
-      return curry(func).apply(undefined, [].concat(store, args));
-    };
-  };
-};
-
-var pureProp = function pureProp(props, obj) {
-  return keys(props).reduce(function (obj, prop) {
-    return hasProp(obj, prop) ? obj[prop] : undefined;
-  });
-};
-
-var prop = curry(pureProp);
-
-var type = function type(obj) {
+var type = exports.type = function type(obj) {
   if (obj === null) {
     return 'null';
   }
@@ -62,7 +30,7 @@ var type = function type(obj) {
   }
 
   if (_typeOf === 'object') {
-    var _type = prop(['obj', 'constructor'], obj);
+    var _type = path(['obj', 'constructor'], obj);
     if (_type === Object) {
       return 'object';
     }
@@ -74,91 +42,47 @@ var type = function type(obj) {
   return _typeOf;
 };
 
-var hasProp = function hasProp(obj, item) {
-  return isAOF(obj) ? keys(obj).indexOf(item) >= 0 : false;
-};
-
-var isType = function isType(obj, _type) {
+var isType = exports.isType = function isType(obj, _type) {
   return type(obj) === _type;
 };
 
-var isString = function isString(obj) {
+var isString = exports.isString = function isString(obj) {
   return isType(obj, 'string');
 };
 
-var isNumber = function isNumber(obj) {
+var isNumber = exports.isNumber = function isNumber(obj) {
   return isType(obj, 'number');
 };
 
-var isArray = function isArray(obj) {
+var isArray = exports.isArray = function isArray(obj) {
   return isType(obj, 'array');
 };
 
-var isObject = function isObject(obj) {
+var isObject = exports.isObject = function isObject(obj) {
   return isType(obj, 'object');
 };
 
-var isFunction = function isFunction(obj) {
+var isFunction = exports.isFunction = function isFunction(obj) {
   return isType(obj, 'function');
 };
 
-var isAOF = function isAOF(obj) {
+var isAOF = exports.isAOF = function isAOF(obj) {
   return ['array', 'object', 'function'].indexOf(type(obj)) >= 0;
 };
 
-var isNull = function isNull(obj) {
+var isNull = exports.isNull = function isNull(obj) {
   return isType(obj, 'null');
 };
 
-var isUndefined = function isUndefined(obj) {
+var isUndefined = exports.isUndefined = function isUndefined(obj) {
   return isType(obj, 'undefined');
 };
 
-var copy = function copy(obj) {
-  if (isAOF(obj)) {
-    return copyObject(obj);
-  }
-  return Object.assign(obj);
-};
-
-var copyObject = function copyObject(obj) {
-  var R = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : 0;
-
-  if (R++ > 42) {
-    throw "Limit recursive exceeded in pytils.copyObject";
-  }
-
-  if (isAOF(obj)) {
-    var nObj = new obj.constructor();
-    map(obj, function (v, k) {
-      return nObj[k] = copyObject(v, R);
-    });
-    return nObj;
-  }
-
-  return obj;
-};
-
-var length = function length(obj) {
-  switch (type(obj)) {
-    case 'number':
-    case 'object':
-      return keys(obj).length;
-
-    case 'string':
-    case 'array':
-    case 'function':
-      return obj.length;
-
-    case 'null':
-    case 'undefined':
-    default:
-      return -1;
-  }
-};
+/* TYPE */
+/* OBJECT */
 
 var _keys = function () {
-  var has = prop(['prototype', 'hasOwnProperty'], Object);
+  var has = path(['prototype', 'hasOwnProperty'], Object);
   if (!isFunction(has)) {
     throw "Cant't get Object.prototype.hasOwnProperty";
   }
@@ -177,7 +101,7 @@ var _keys = function () {
   };
 }();
 
-var keys = function keys(obj) {
+var keys = exports.keys = function keys(obj) {
   switch (type(obj)) {
     case 'number':
       obj = String(obj);
@@ -197,7 +121,7 @@ var keys = function keys(obj) {
   }
 };
 
-var values = function values(obj) {
+var values = exports.values = function values(obj) {
   switch (type(obj)) {
     case 'number':
       return String(obj).split('').map(function (n) {
@@ -209,8 +133,8 @@ var values = function values(obj) {
 
     case 'object':
     case 'function':
-      return map(obj, function (k) {
-        return obj[k];
+      return map(obj, function (key) {
+        return obj[key];
       });
 
     case 'array':
@@ -221,18 +145,137 @@ var values = function values(obj) {
   }
 };
 
-var toObject = function toObject(input) {
-  if (isArray(input) && length(v) <= 0) {
-    return {};
+var _copy = function _copy(obj) {
+  var R = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : 0;
+
+  if (R++ > 42) {
+    throw "Limit recursive exceeded in pytils.copyObject";
   }
 
-  return copy(input).reduce(function (obj, v, k) {
-    obj[v] = k;
-    return obj;
-  }, {});
+  if (isAOF(obj)) {
+    var nObj = new obj.constructor();
+    map(obj, function (v, k) {
+      return nObj[k] = copyObject(v, R);
+    });
+    return nObj;
+  }
+
+  return obj;
 };
 
-var arrayDiff = function arrayDiff(list, compare) {
+var copy = exports.copy = function copy(obj) {
+  if (isAOF(obj)) {
+    return copyObject(obj);
+  }
+  return Object.assign(obj);
+};
+
+var hasProp = exports.hasProp = function hasProp(obj, item) {
+  return isAOF(obj) ? keys(obj).indexOf(item) >= 0 : false;
+};
+
+var length = exports.length = function length(obj) {
+  switch (type(obj)) {
+    case 'number':
+    case 'object':
+      return keys(obj).length;
+
+    case 'string':
+    case 'array':
+    case 'function':
+      return obj.length;
+
+    case 'null':
+    case 'undefined':
+    default:
+      return -1;
+  }
+};
+
+/* OBJECT */
+/* RAMDA LIKE */
+
+var compose = exports.compose = function compose() {
+  for (var _len = arguments.length, funcs = Array(_len), _key = 0; _key < _len; _key++) {
+    funcs[_key] = arguments[_key];
+  }
+
+  return function (input) {
+    return funcs.reverse().reduce(function (obj, fx) {
+      return fx(obj);
+    }, input);
+  };
+};
+
+var curry = exports.curry = function curry(func) {
+  return function () {
+    for (var _len2 = arguments.length, args = Array(_len2), _key2 = 0; _key2 < _len2; _key2++) {
+      args[_key2] = arguments[_key2];
+    }
+
+    if (args.length >= func.length) {
+      return func.apply(undefined, args);
+    }
+    return function () {
+      for (var _len3 = arguments.length, nextArgs = Array(_len3), _key3 = 0; _key3 < _len3; _key3++) {
+        nextArgs[_key3] = arguments[_key3];
+      }
+
+      return curry(func).apply(undefined, _toConsumableArray(args.concat(nextArgs)));
+    };
+  };
+};
+
+var path = exports.path = curry(function (path) {
+  return function (obj) {
+    return path.reduce(function (acc, item) {
+      return acc !== null && acc !== undefined && (typeof acc === 'undefined' ? 'undefined' : _typeof(acc)) === 'object' ? acc[item] : undefined;
+    }, obj);
+  };
+});
+
+var map = exports.map = curry(function (func) {
+  return function (list) {
+    return isArray(list) ? list.map(func) : isObject(list) ? keys(list).map(function (key) {
+      return func(list[key], key);
+    }) : undefined;
+  };
+});
+
+var reduce = exports.reduce = function reduce(func, obj) {
+  return function (list) {
+    return isArray(list) ? list.reduce(func, obj) : isObject(list) ? keys(list).reduce(function (obj, key) {
+      return func(obj, list[key], key);
+    }, obj) : undefined;
+  };
+};
+
+var uniqWith = exports.uniqWith = function uniqWith(comparator, list) {
+  var outputList = [];
+  map(function (itemA) {
+    var equals = map(function (itemB) {
+      return comparator(itemA, itemB);
+    })(outputList).filter(function (item) {
+      return item;
+    });
+
+    equals && equals.length === 0 && outputList.push(itemA);
+  })(list);
+  return outputList;
+};
+
+var uniq = exports.uniq = function uniq(list) {
+  return values(createObj(list));
+};
+
+var eq1True = exports.eq1True = function eq1True(list) {
+  return isArray(list) && list.length === 1 && list[0] === true;
+};
+
+/* RAMDA LIKE */
+/* PYTILS */
+
+var arrayDiff = exports.arrayDiff = function arrayDiff(list, compare) {
   if (length(compare) <= 0) {
     return copy(list);
   }
@@ -246,32 +289,43 @@ var arrayDiff = function arrayDiff(list, compare) {
   return keys(obj);
 };
 
-var map = function map(obj, func) {
-  return keys(obj).map(function (k) {
-    return func(obj[k], k, obj);
-  });
+var toObject = exports.toObject = function toObject(input) {
+  if (isArray(input) && length(v) <= 0) {
+    return {};
+  }
+  return input.reduce(function (obj, v, k) {
+    obj[v] = copy(k);
+    return obj;
+  }, {});
 };
 
-module.exports = {
-  compose: compose,
-  curry: curry,
-  prop: prop,
-  type: type,
-  isType: isType,
-  isString: isString,
-  isNumber: isNumber,
-  isArray: isArray,
-  isObject: isObject,
-  isFunction: isFunction,
-  isAOF: isAOF,
-  isNull: isNull,
-  isUndefined: isUndefined,
-  copy: copy,
-  length: length,
-  keys: keys,
-  values: values,
-  hasProp: hasProp,
-  map: map,
-  toObject: toObject,
-  arrayDiff: arrayDiff
+var invertObj = exports.invertObj = function invertObj(input) {
+  return reduce(function (obj, val, key) {
+    obj[val] = key;
+    return obj;
+  }, {})(input);
 };
+
+var ojbFromVals = exports.ojbFromVals = function ojbFromVals(arrKeys) {
+  return arrKeys.reduce(function (obj, val) {
+    obj[val] = val;
+    return obj;
+  }, {});
+};
+
+var translate = exports.translate = curry(function (dictionary) {
+  return function (original) {
+    return reduce(function (obj, ori, des) {
+      obj[des] = path([ori], original);
+      return obj;
+    }, {})(dictionary);
+  };
+});
+
+var uniqObject = exports.uniqObject = function uniqObject(A, B) {
+  return compose(eq1True, uniq, map(function (key) {
+    return path([key], A) === path([key], B);
+  }), keys)(A);
+};
+
+/* PYTILS */
