e8a0c9fa365dbf432bb6ab5c0495b7c1c1207e94
heyder.dias <heyder.dias@b2wdigital.com>
2017-04-26 17:45:22 -0300

    group object

--- src/main.js
+++ src/main.js
@@ -6,7 +6,8 @@
 */
 
 /* TYPE */
-const type = obj => {
+
+export const type = obj => {
   if (obj === null) {
     return 'null'
   }
@@ -32,24 +33,146 @@ const type = obj => {
   return _typeOf
 }
 
-const isType = (obj, _type) => type(obj) === _type
+export const isType = (obj, _type) => type(obj) === _type
 
-const isString = obj => isType(obj, 'string')
+export const isString = obj => isType(obj, 'string')
 
-const isNumber = obj => isType(obj, 'number')
+export const isNumber = obj => isType(obj, 'number')
 
-const isArray = obj => isType(obj, 'array')
+export const isArray = obj => isType(obj, 'array')
 
-const isObject = obj => isType(obj, 'object')
+export const isObject = obj => isType(obj, 'object')
 
-const isFunction = obj => isType(obj, 'function')
+export const isFunction = obj => isType(obj, 'function')
 
-const isAOF = obj => ['array', 'object', 'function'].indexOf(type(obj)) >= 0
+export const isAOF = obj => ['array', 'object', 'function'].indexOf(type(obj)) >= 0
 
-const isNull = obj => isType(obj, 'null')
+export const isNull = obj => isType(obj, 'null')
 
-const isUndefined = obj => isType(obj, 'undefined')
+export const isUndefined = obj => isType(obj, 'undefined')
+
 /* TYPE */
+/* OBJECT */
+
+const _keys = (() => {
+  const has = prop(['prototype', 'hasOwnProperty'], Object)
+  if (!isFunction(has)){
+    throw "Cant't get Object.prototype.hasOwnProperty"
+  }
+
+  return obj => {
+    if (isAOF(obj)) {
+      const props = []
+      for (let p in obj) {
+        props.push(p)
+      }
+      return props
+        .filter(
+          p => has.call(obj, p))
+    }
+    return []
+  }
+})()
+
+export const keys = obj => {
+  switch(type(obj)) {
+    case 'number':
+      obj = String(obj)
+
+    case 'string':
+      obj = obj.split('')
+
+    case 'array':
+    case 'object':
+    case 'function':
+      return _keys(obj)
+
+    case 'null':
+    case 'undefined':
+    default:
+      return []
+  }
+}
+
+export const values = obj => {
+  switch(type(obj)) {
+    case 'number':
+      return String(obj)
+        .split('')
+        .map(n => parseInt(n))
+
+    case 'string':
+      return obj.split('')
+
+    case 'object':
+    case 'function':
+      return map(obj, key => obj[key])
+
+    case 'array':
+    case 'null':
+    case 'undefined':
+    default:
+      return []
+  }
+}
+
+const _copy = (obj, R = 0) => {
+  if (R++ > 42) {
+    throw "Limit recursive exceeded in pytils.copyObject"
+  }
+
+  if(isAOF(obj)) {
+    const nObj = new obj.constructor()
+    map(
+      obj,
+      (v, k) => nObj[k] = copyObject(v, R))
+    return nObj
+  }
+
+  return obj
+}
+
+export const copy = obj => {
+  if(isAOF(obj)) {
+    return copyObject(obj)
+  }
+  return Object.assign(obj)
+}
+
+export const hasProp = (obj, item) => isAOF(obj)
+  ? keys(obj).indexOf(item) >= 0
+  : false
+
+export const length = obj => {
+  switch(type(obj)) {
+    case 'number':
+    case 'object':
+      return keys(obj).length
+
+    case 'string':
+    case 'array':
+    case 'function':
+      return obj.length
+
+    case 'null':
+    case 'undefined':
+    default:
+      return -1
+  }
+}
+
+export const toObject = input => {
+  if (isArray(input) && length(v) <= 0) {
+    return {}
+  }
+  return input
+    .reduce((obj, v, k) => {
+      obj[v] = copy(k)
+      return obj
+    }, {})
+}
+
+/* OBJECT */
 
 const compose = (...args) => {
   return input => args
@@ -73,127 +196,6 @@ const pureProp = (props, obj) => keys(props)
     : undefined)
 
 const prop = curry(pureProp)
-
-
-
-const hasProp = (obj, item) => isAOF(obj)
-  ? keys(obj).indexOf(item) >= 0
-  : false
-
-const copy = obj => {
-  if(isAOF(obj)) {
-    return copyObject(obj)
-  }
-  return Object.assign(obj)
-}
-
-const copyObject = (obj, R = 0) => {
-  if (R++ > 42) {
-    throw "Limit recursive exceeded in pytils.copyObject"
-  }
-
-  if(isAOF(obj)) {
-    const nObj = new obj.constructor()
-    map(
-      obj,
-      (v, k) => nObj[k] = copyObject(v, R))
-    return nObj
-  }
-
-  return obj
-}
-
-const length = obj => {
-  switch(type(obj)) {
-    case 'number':
-    case 'object':
-      return keys(obj).length
-
-    case 'string':
-    case 'array':
-    case 'function':
-      return obj.length
-
-    case 'null':
-    case 'undefined':
-    default:
-      return -1
-  }
-}
-
-const _keys = (() => {
-  const has = prop(['prototype', 'hasOwnProperty'], Object)
-  if (!isFunction(has)){
-    throw "Cant't get Object.prototype.hasOwnProperty"
-  }
-
-  return obj => {
-    if (isAOF(obj)) {
-      const props = []
-      for (let p in obj) {
-        props.push(p)
-      }
-      return props
-        .filter(
-          p => has.call(obj, p))
-    }
-    return []
-  }
-})()
-
-const keys = obj => {
-  switch(type(obj)) {
-    case 'number':
-      obj = String(obj)
-
-    case 'string':
-      obj = obj.split('')
-
-    case 'array':
-    case 'object':
-    case 'function':
-      return _keys(obj)
-
-    case 'null':
-    case 'undefined':
-    default:
-      return []
-  }
-}
-
-const values = obj => {
-  switch(type(obj)) {
-    case 'number':
-      return String(obj)
-        .split('')
-        .map(n => parseInt(n))
-
-    case 'string':
-      return obj.split('')
-
-    case 'object':
-    case 'function':
-      return map(obj, k => obj[k])
-
-    case 'array':
-    case 'null':
-    case 'undefined':
-    default:
-      return []
-  }
-}
-
-const toObject = input => {
-  if (isArray(input) && length(v) <= 0) {
-    return {}
-  }
-
-  return copy(input)
-    .reduce((obj, v, k) => {
-      obj[v] = k
-      return obj
-    }, {})
-}
 
 const arrayDiff = (list, compare) => {
   if (length(compare) <= 0) {
